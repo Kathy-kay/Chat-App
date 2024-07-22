d1617bedca4aa39acb5a4b3f97ecfa38302299f6
Kafayat <adigunkafy27@gmail.com>
2024-07-22 15:04:41 +0100

    feat: implement routing and design the authentication page (signin and signup)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
-import { Button } from "@/components/ui/button"
+import { Routes,Route } from "react-router-dom"
+import Auth from "./pages/auth"
+import Profile from "./pages/profile"
+import Chat from "./pages/chat"
+
 
 
 function App() {
   
   return (
-   <Button>Click me</Button>
+   <Routes>
+     <Route path="/auth" element={<Auth/>}/>
+     <Route path="/profile" element={<Profile/>}/>
+     <Route path="/chat" element={<Chat/>}/>
+   </Routes>
   )
 }
 
