40d3e7875926e3e64a01241864d9acdca7156845
Kafayat <adigunkafy27@gmail.com>
2024-08-05 16:24:05 +0100

    restyled the signup page

--- client/src/_auth/AuthLayout.jsx
+++ client/src/_auth/AuthLayout.jsx
@@ -1,8 +1,20 @@
+import { Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
   return (
-    <div>AuthLayout</div>
-  )
-}
+    <>
+      <section className="flex flex-1 justify-center items-center flex-col py-10 ">
+        <div className="bg-white border-2 px-20  py-10 border-white text-opacity-90 rounded-3xl max-xl:shadow-2xl md:w-[90vw] lg:w-[70vw] xl:w-full">
+          <Outlet />
+        </div>
+      </section>
+      <img
+        src="/assets/images/side-img.svg"
+        alt="authentication side image"
+        className="hidden xl:block h-screen w-1/2 object-cover no-repeat"
+      />
+    </>
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
--- client/src/_auth/forms/SignupForm.jsx
+++ client/src/_auth/forms/SignupForm.jsx
@@ -1,9 +1,58 @@
-
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const SignupForm = () => {
+  const {register, handleSubmit, formState: {errors}} = useForm()
   return (
-    <div>SignupForm</div>
-  )
-}
+    <div className="flex items-center justify-center flex-col">
+      <div className="flex flex-col md:justify-center   gap-7 h-full w-full">
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex items-center justify-center">
+            <h1 className="font-bold text-5xl md:text-6xl">Welcome</h1>
+            <img src="/assets/images/victory.svg" alt="victory emoji" className="h-[100px]" />
+          </div>
+          <p className="font-medium text-center text-base leading-[140%] text-[#180161]">
+            Fill in the details to get started with the chat app
+          </p>
+        </div>
+        <form onSubmit={handleSubmit()} className="flex flex-col w-full gap-5 mt-4">
+          <input 
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: "Invalid email address",
+            }
+          })}
+          placeholder="Email"
+          type="email"
+          className={`rounded-2xl p-6  border-2 ${errors.email ? "border-red-500" : "border-[#180161]"}`}
+          />
+          {errors.email && <p className="text-red-500 mt-5">{errors.email.message}</p>}
+          <input 
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 6,
+              message: "Password must be atleast 6 characters"
+            }
+          })}
+          placeholder="Password"
+          type="password"
+          className={`rounded-2xl p-6 outline-none  border-2 ${errors.email ? "border-red-500" : "border-[#180161]"}`}
+          />
+           {errors.password && <p className="text-red-500 mt-5">{errors.password.message}</p>}
 
-export default SignupForm
+           <Button className="rounded-2xl p-6 bg-[#180161]/70 border-none hover:bg-[#180161]">Signup</Button>
+
+           <p className="text-[14px] font-normal leading-[140%] text-center">Already have an account?
+            <Link to="/sign-in" className="text-[#180161] font-semibold text-base">Signin</Link>
+           </p> 
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignupForm;
