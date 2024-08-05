c9bf8633449590872243884e1750f2751f7e63db
Kafayat <adigunkafy27@gmail.com>
2024-08-05 10:56:27 +0100

    create a profile file and it routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,41 @@
 import { Routes,Route } from "react-router-dom"
-import Auth from "./_auth"
+import AuthLayout from "./_auth/AuthLayout"
+import SignupForm from "./_auth/forms/SignupForm"
+import SigninForm from "./_auth/forms/SigninForm"
+import EmailVerify from "./_auth/forms/EmailVerify"
+import OtpVerify from "./_auth/forms/OtpVerify"
+import ResetPassword from "./_auth/forms/ResetPassword"
+import RootLayout from "./root/RootLayout"
+import { Home } from "lucide-react"
+import Chatroom from "./root/pages/Chatroom"
 import Profile from "./root/pages/Profile"
-import Chat from "./root/pages/chat"
+
 
 
 
 function App() {
   
   return (
-   <Routes>
-     <Route path="/auth" element={<Auth/>}/>
-     <Route path="/profile" element={<Profile/>}/>
-     <Route path="/chat" element={<Chat/>}/>
-   </Routes>
+    <main className="flex h-screen">
+      <Routes>
+
+        {/* public Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-up" element={<SignupForm />} />
+          <Route path="/sign-in" element={<SigninForm />}/>
+          <Route path="/forgotpassword/email-verify" element={<EmailVerify />}/>
+          <Route path="/forgotpassword/otp-verify" element={<OtpVerify />}/>
+          <Route path="/reset-password" element={<ResetPassword />}/>
+        </Route>
+
+        {/* privete Routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home/>} />
+          <Route path="/chat/message" element={<Chatroom />}/>
+          <Route path="/profile" element={<Profile />}/>
+        </Route>
+      </Routes>
+    </main>
   )
 }
 
